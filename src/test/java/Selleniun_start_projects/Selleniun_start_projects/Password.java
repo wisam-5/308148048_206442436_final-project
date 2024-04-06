4d7cf56f3ecc64c1c921aed1b733178ef52f70f0
wisam <wisam@DESKTOP-I22KP5J>
2024-04-06 19:48:26 +0300

    test1-password+ triangle class created

--- src/test/java/Selleniun_start_projects/Selleniun_start_projects/Password.java
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/Password.java
@@ -1,5 +1,41 @@
 package Selleniun_start_projects.Selleniun_start_projects;
 
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import java.util.concurrent.TimeUnit;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.lang.System;
+
 public class Password {
+	static WebDriver browser;
+
+	@Before
+	public void setup() {
+
+		System.setProperty("webdriver.gecko.driver", "C:\\driver\\geckodriver.exe");
+		browser = new FirefoxDriver();
+		browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		String url = "https://testpages.eviltester.com/styled/apps/7charval/simple7charvalidation.html";
+		browser.get(url);
+
+	}
+	@Test
+	public void t1() {
+		browser.findElement(By.name("characters")).sendKeys("wisamha");
+		browser.findElement(By.name("validate")).click();
+		
+		String expected = "Valid Value";
+		String Result = browser.findElement(By.name("validation_message")).getAttribute("value");
+		assertEquals(expected, Result);
+	}
 
 }
+
